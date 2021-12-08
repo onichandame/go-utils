4401f7deaa51673cc910143a50fb6072d930ae8d
onichandame <zxinmyth@gmail.com>
2021-12-08 10:47:35 +0000

    feat: json marshal to map

--- json.go
+++ json.go
@@ -7,3 +7,9 @@ func UnmarshalJSONFromMap(m map[string]interface{}, ent interface{}) {
 	Assert(err)
 	Assert(json.Unmarshal(bytes, ent))
 }
+
+func MarshalJSONToMap(ent interface{}, m map[string]interface{}) {
+	bytes, err := json.Marshal(ent)
+	Assert(err)
+	json.Unmarshal(bytes, &m)
+}
--- json_test.go
+++ json_test.go
@@ -8,13 +8,11 @@ import (
 )
 
 func TestJSON(t *testing.T) {
+	type Obj struct {
+		ID       int    `json:"id"`
+		Children []*Obj `json:"children"`
+	}
 	t.Run("can unmarshal from map", func(t *testing.T) {
-		type Obj struct {
-			ID       int `json:"id"`
-			Children []*struct {
-				ID int `json:"id"`
-			} `json:"children"`
-		}
 		var obj Obj
 		assert.NotPanics(t, func() {
 			goutils.UnmarshalJSONFromMap(map[string]interface{}{"id": 1, "children": []interface{}{map[string]interface{}{"id": 2}}}, &obj)
@@ -22,5 +20,19 @@ func TestJSON(t *testing.T) {
 		assert.Equal(t, 1, obj.ID)
 		assert.Len(t, obj.Children, 1)
 		assert.Equal(t, 2, obj.Children[0].ID)
+	})
+	t.Run("can marshal to map", func(t *testing.T) {
+		var obj Obj
+		obj.ID = 1
+		obj.Children = make([]*Obj, 0)
+		obj.Children = append(obj.Children, &Obj{ID: 2})
+		m := make(map[string]interface{})
+		goutils.MarshalJSONToMap(&obj, m)
+		assert.EqualValues(t, 1, m["id"])
+		assert.Len(t, m["children"], 1)
+		assert.IsType(t, make([]interface{}, 0), m["children"])
+		child := m["children"].([]interface{})[0]
+		assert.IsType(t, make(map[string]interface{}), child)
+		assert.EqualValues(t, 2, child.(map[string]interface{})["id"])
 	})
 }
